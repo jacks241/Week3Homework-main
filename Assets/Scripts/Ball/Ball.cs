98bf64e84859a8cb95ccae267b5b545b2cfdc704
jacks241 <jacks241@miamioh.edu>
2021-09-16 17:32:51 -0400

    Initial commit

---
+++ Assets/Scripts/Ball/Ball.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    //TODO: Add code to move ball along with code to delete pieces upon colliding with one
+    //Ball should only move once its been launched 
+    [SerializeField]
+    private Rigidbody2D rigidBody = null;
+
+    [SerializeField]
+    private float speed = 1.0f;
+
+    private Vector3 currentDirection = Vector3.zero;
+
+    private void Awake()
+    {
+        //currentDirection = new Vector3(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f), 0);
+        currentDirection = new Vector3(Random(), Random(), 0).normalized;
+    }
+    
+
+    void Start()
+    {
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        currentDirection = Vector2.Reflect(currentDirection, collision.contacts[0].normal);
+    }
+
+    void Update()
+    {
+        transform.Translate(currentDirection * Time.deltaTime * speed);
+    }
+
+    private int Random()
+    {
+        return UnityEngine.Random.Range(0, 2) == 0 ? -1 : 1;
+    }
+}//end of code
---
+++ Assets/Scripts/Paddle/Paddle.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    //TODO
+    // Move paddle left and right using keyboard keys, mapping is up to you
+    // Paddle should be able to launch the ball upon space bar being pressed
+    // A launched ball will then bounce around, changing its direction upon any collision
+    [SerializeField]
+    private float speed = 1.0f;
+    void Start ()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed, Space.World);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * speed, Space.World);
+        }
+
+        var xPosition = Mathf.Clamp(transform.position.x, -7.9f, 7.9f);
+        transform.position = new Vector3(xPosition, transform.position.y, transform.position.z);
+    }
+} //end of code
